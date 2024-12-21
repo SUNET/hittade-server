d7c19f5c55d5f23c382a128a33f664db7182252a
Simon <contact@smn.codes>
2024-12-21 15:48:07 +0100

    Configure SASS in web container

---
+++ .docker/scripts/setup-sass.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+VERSION=1.83.0;
+
+URL="https://github.com/sass/dart-sass/releases/download/${VERSION}/dart-sass-${VERSION}-linux-x64.tar.gz";
+
+mkdir -p /opt/dart-sass;
+
+curl -sL $URL | tar zx -C /opt;
+
+ln -s /opt/dart-sass/sass /usr/local/bin/sass;
