37220a953d94a3329ac2374218c0bace6aba1f5b
Simon <contact@smn.codes>
2025-05-22 15:52:18 +0200

    Type for start page request param

--- start_page/views.py
+++ start_page/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 
-def start_page(request):
+def start_page(request: HttpRequest):
     template = "authenticated" if request.user.is_authenticated else "public"
 
     return render(request, f"start_page/{template}.html")
